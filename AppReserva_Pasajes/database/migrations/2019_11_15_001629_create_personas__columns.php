8ae09257648b747c84069c3e4c69221e91190665
Adhair <adhaircC@outlook.com>
2019-11-14 19:47:18 -0500

    columns persona

---
+++ AppReserva_Pasajes/database/migrations/2019_11_15_001629_create_personas__columns.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonasColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Personas', function (Blueprint $table) {
+            $table->string('nombre');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Personas', function (Blueprint $table) {
+            $table->dropColumn('nombre');
+            //
+        });
+    }
+}
